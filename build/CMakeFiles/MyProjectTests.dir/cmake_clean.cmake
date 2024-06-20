f22cdcf5c72031558f977b755bfb7ed1919f748c
AndreySagiev <asagiev-2005@mail.ru>
2024-06-20 14:22:04 +0300

    Final version

---
+++ build/CMakeFiles/MyProjectTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProjectTests.dir/src/func_test.cpp.obj"
+  "CMakeFiles/MyProjectTests.dir/src/func_test.cpp.obj.d"
+  "MyProjectTests.exe"
+  "MyProjectTests.exe.manifest"
+  "MyProjectTests.pdb"
+  "libMyProjectTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProjectTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projectsfml/Strateg/Strateg
+# Build directory: C:/Projectsfml/Strateg/Strateg/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyProjectTests "C:/Projectsfml/Strateg/Strateg/build/MyProjectTests.exe")
+set_tests_properties(MyProjectTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Projectsfml/Strateg/Strateg/CMakeLists.txt;22;add_test;C:/Projectsfml/Strateg/Strateg/CMakeLists.txt;0;")
